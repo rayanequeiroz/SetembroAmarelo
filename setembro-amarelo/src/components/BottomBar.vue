d8d93af228b1d97de7f553d20eb988d35aee689f
Rayane Queiroz <rayanequeirozrj@gmail.com>
2021-10-02 14:27:37 -0300

    Bottom Bar adicionado

---
+++ setembro-amarelo/src/components/BottomBar.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="bottom-bar">
+    <v-bottom-navigation
+    :value="value"
+    grow
+    app>
+
+    <router-link to="/">
+    <v-btn>
+      <span>Início</span>
+
+      <v-icon>mdi-home</v-icon>
+    </v-btn>
+    </router-link>
+
+  <router-link to ="/informacoes">
+    <v-btn>
+      <span>Informações</span>
+
+      <v-icon>mdi-information</v-icon>
+    </v-btn>
+</router-link>
+
+  <router-link to="/ajuda">
+    <v-btn>
+      <span>Ajuda</span>
+
+      <v-icon>mdi-help</v-icon>
+    </v-btn>
+    </router-link>
+
+  </v-bottom-navigation>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'BottomBar',
+
+    data: () => ({
+      value: 0,
+    }),
+  }
+</script>
+
+<style scoped>
+
+.bottom-bar a {
+  text-decoration: none;
+  display: flex;
+  flex-grow: 1;
+  justify-content: center;
+  color: #d8ac00;
+}
+</style>
