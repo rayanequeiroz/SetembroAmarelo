ba0a9811d9747b096e47072ff49a3fa53bb32fed
Rayane Queiroz <rayanequeirozrj@gmail.com>
2021-10-02 17:30:47 -0300

    Inicio

--- setembro-amarelo/src/views/Inicio.vue
+++ setembro-amarelo/src/views/Inicio.vue
@@ -1,9 +1,14 @@
 <template>
   <v-container>
-     <h2>"Setembro Amarelo:A importância de falar sobre prevenção de suicídio"</h2>
-     <v-img src="https://images.app.goo.gl/JF6kM9HRwGfyEnqv6"></v-img>
+     <p class="text-home text-justify mt-5 ml-4 mr-4">O Setembro Amarelo é uma campanha criada com o intuito de informar as pessoas sobre o suicídio, uma prática normalmente motivada pela depressão. Mesmo com tantos casos notórios, crescentes a cada ano, ainda existe uma expressiva barreira para falar sobre o problema.</p>
+<div class="btn-contato text-center">
+<router-link to="/informacoes">
+        <v-btn outlined rounded color="#f9d71c" elevation="2">
+          <span class="text-btn"><strong>Saiba mais</strong></span>
+        </v-btn>
+      </router-link>
+    </div>
 
-     <p>O Setembro Amarelo é uma campanha criada com o intuito de informar as pessoas sobre o suicídio, uma prática normalmente motivada pela depressão. Mesmo com tantos casos notórios, crescentes a cada ano, ainda existe uma expressiva barreira para falar sobre o problema.</p>
     </v-container>
 </template>
 
@@ -17,6 +22,13 @@
     },
   }
 </script>
+
+<style scoped>
+.btn-contato a {
+    text-decoration: none;
+  }
+
+</style>
  
 
     
