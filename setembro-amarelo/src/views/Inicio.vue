33e1d6287ac4174310217705819688f9f8cab6be
Evelyn Pires da Silva <evelyn.pires2012@hotmail.com>
2021-10-02 16:03:13 -0300

    escape

--- setembro-amarelo/src/views/Inicio.vue
+++ setembro-amarelo/src/views/Inicio.vue
@@ -1,14 +1,17 @@
 <template>
-  <div>
-    <h1>Oi</h1>
-  </div>
+  <v-container>
+     <h2>"Setembro Amarelo:A importância de falar sobre prevenção de suicídio"</h2>
+     <v-img src="https://images.app.goo.gl/JF6kM9HRwGfyEnqv6"></v-img>
+
+     <p>O setembro ama</p>
+    </v-container>
 </template>
 
 <script>
   
   export default {
     name: 'Inicio',
-
+    
     components: {
       //
     },
